523cd656230c838ad9ba2367489ecab28e5b7685
Anthony Lee <ant.lee813@gmail.com>
2017-08-06 15:21:28 -0700

    Initial commit

---
+++ automate.py
@@ -0,0 +1,22 @@
+""" read input file and automatically replace codewords with selected functions
+    and creates new output file
+
+  written by: Anthony Lee
+              github.com/inc1t3Ful
+
+  Last edit: 6 Aug 2017
+
+"""
+# 1st iteration
+in_file = open("testfile.txt", "rt")    # open input file and Read Text
+contents = in_file.read()               # read entire file into string variable
+in_file.close()                         # close file
+print(contents)                         # print
+
+
+# open input file and Read Text, then close. Print contents
+with open('testfile.txt', 'rt') as in_file:
+    # store each line in string variable "line"
+    for line in in_file:
+        # print line
+        print(line)
