db0e9091c84d9c78b1e216cdbc085ec4848f28c2
Anthony Lee <ant.lee813@gmail.com>
2018-09-09 18:47:26 -0700

    improved output format
    ** small fix. now extra \n in first output, but should be fine otherwise

--- automate.py
+++ automate.py
@@ -5,7 +5,7 @@
   written by: Anthony Lee
               github.com/inc1t3Ful
 
-  Last edit: 6 Jan 2018
+  Last edit: 9 Sep 2018
 
 """
 # import python regular expression module to strengthen search capabilities
@@ -24,14 +24,18 @@ with open('testscript_input.txt', 'rtw') as in_file:
     # removing rstrip for \n does not work for better formatting [22 Sep 2017 edit10]
         lines.append(line.rstrip('\n'))
 
-    d_occur = re.sub(r"\w\w\wd\w\w\w", "delete\n", str(lines))
-    a_occur = re.sub(r"\w\w\wa\w\w\w", "add\n", str(d_occur))
-    c_occur = re.sub(r"\w\w\wc\w\w\w", "remove\n", str(a_occur))
-    minus_occur = re.sub(r"---", "add\n", str(c_occur))
+    d_occur = re.sub(r"\w\w\wd\w\w\w", "\ndelete", str(lines))
+    a_occur = re.sub(r"\w\w\wa\w\w\w", "\nadd", str(d_occur))
+    c_occur = re.sub(r"\w\w\wc\w\w\w", "\nremove", str(a_occur))
+    minus_occur = re.sub(r"---", "\nadd", str(c_occur))
 
+# remove all special characters excluding newline
 pattern = re.sub(r'([^\s\w]|_)+', '', minus_occur)
-# print pattern
+# to test in terminal
+print(pattern)
+
 # test output for minus_occur (22 Sep 2017 edit12)
+# output to textfile
 test = open("testscript_output.txt", "w")
 test.write(pattern)
 test.close()
